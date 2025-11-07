16bfab56aa0f6a817ed29fe827e57ad515d19222
BaoVy2810 <vytnb234112e@st.uel.edu.vn>
2025-11-07 12:22:53 +0700

    fix cart icon

--- EcoFit/public/js/cart.js
+++ EcoFit/public/js/cart.js
@@ -58,7 +58,7 @@ function updateCartBadge() {
     window.parent.postMessage({
       action: 'updateCartBadge',
       count: totalItems
-    }, '*');
+    }, 'https://ecofit-store.netlify.app');
   }
 }
 
